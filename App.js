0769d08d426a1e1dcbc8ee71cd6eb6bdee1d2adc
mmoreira10 <matheus_moreira_1095@hotmail.com>
2020-08-15 19:46:14 -0300

    Commit aula 2 e 3

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Page from './views/Page';
 
 export default function App() {
+  const props = {
+    empresa: 'TrackingON',
+    name: 'Matheus'
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bem-vindo!</Text>
+      <Page {...props} />
+      {/* <Page empresa='Programação'/>
+      <Page empresa='Development'/> */}
       <StatusBar style="auto" />
     </View>
   );
---
+++ views/Page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Page(props) {
+    return (
+        <View>
+            <Text>O nome da empresa é {props.empresa} e seu funcionário é {props.name}</Text>
+        </View>
+    );
+}
